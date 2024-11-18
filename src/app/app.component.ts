3ae441d59ea5ca8ea667a9dba404e2693c951713
Seb Leduc <sebas.leduc@gmail.com>
2024-11-18 13:31:35 -0500

    Added possibility of displaying many charts at once

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChartComponent } from './chart/chart.component';
 import customerData from '../../userData.json';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChartComponent],
+  imports: [RouterOutlet, ChartComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
